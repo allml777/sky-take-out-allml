0542415ae58fb875df14492876a0c22133b2d67b
ALLML <1051322819@qq.com>
2025-04-27 15:05:48 +0800

    解决冲突

--- sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -2,6 +2,7 @@ package com.sky.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,5 +12,8 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class AutoFillAspect {
-
+    /**
+     * 切入点
+     */
+//    @Pointcut("execution()")
 }
